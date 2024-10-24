efdd2283a2d1d15520e2fc25fb9120ea35c3efac
kaa-dan <nithinraj.dev@gmail.com>
2024-10-24 21:50:42 +0530

    dependency injection

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,17 +8,19 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
   @Get() //get/users
   findAll(@Query('role') role?: 'INTERN') {
-    return [role];
+    return this.usersService.findAll(role);
   }
 
   @Get(':id') //get/users/:id
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.usersService.findOne(+id);
   }
 
   @Post() //post/users
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,14 +7,48 @@ export class UsersService {
     { id: 1, name: 'nithin', lastname: 'rs', role: 'INTERN' },
     { id: 1, name: 'nithin', lastname: 'sg', role: 'DEVELOPER' },
   ];
+
+  //   find all user
   findAll(role?: 'INTERN' | 'ENGINEER') {
     if (role) {
       return this.users.filter((user) => user.role === role);
     }
     return this.users;
   }
+
+  //findone user
   findOne(id: number) {
-    
     return this.users.filter((user) => user.id === id);
+  }
+
+  create(user: { id: number; name: string; lastname: string; role: string }) {
+    const HIGHEST_USER_ID = [...this.users.sort((a, b) => b.id - a.id)];
+    const newUser = {
+      id: HIGHEST_USER_ID[0].id,
+      ...user,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(
+    id: number,
+    updateUser: {
+      name?: string;
+      lastname?: string;
+      role?: 'INTERN' | 'DEVELOPER';
+    },
+  ) {
+    this.users = this.users.map((singleUser) => {
+      if (singleUser.id === id) return { ...singleUser, ...updateUser };
+      return singleUser;
+    });
+    return this.findOne(id);
+  }
+
+  delete(id: number) {
+    const removedUser = this.findOne(id);
+    this.users = this.users.filter((user) => user.id !== id);
+    return removedUser;
   }
 }
